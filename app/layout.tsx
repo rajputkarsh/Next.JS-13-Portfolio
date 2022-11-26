5772f9977c62451117b8b9f5f6d25a74b6ea6e57
utkarsh <utkarsh1@appinventiv.com>
2022-11-26 11:33:44 +0530

    add raw html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import "tailwindcss/tailwind.css";
+import Navbar from '../components/navbar/Navbar';
 import "../styles/globals.css";
 import { getTheme } from '../utils/theme';
 
@@ -11,6 +12,9 @@ const RootLayout = ({ children } : { children: React.ReactNode }) => {
     return (
         <html lang="en" className={theme}>
             <body>
+                <header>
+                    <Navbar />                    
+                </header>
                 {children}
             </body>
         </html>
