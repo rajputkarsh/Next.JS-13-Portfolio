c9bee0be05d5aac767000b235393eb1a6b99944f
Utkarsh <r.utkarsh.0010@gmail.com>
2022-12-25 15:14:38 +0530

    split code for skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,14 @@
 "use client";
 
 import { useContext } from "react";
-import { Grid, List, ListItem, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { ThemeContext, ThemeContextType } from "../context/theme";
 import { getThemeFromContext } from "../utils/helper/theme";
 import { THEME } from "../utils/theme";
-import { STATIC_CONTENT } from "../utils/constant";
 
-import { Icon } from '@iconify/react';
-
-import style from './homepage.module.css'
-import { titleCase } from "../utils/helper/text";
-import Image from "next/image";
 import Introduction from "@components/introduction/Introduction";
-import { SECTION_TITLE, VERTICALLY_HORIZONTALLY_CENTER } from "@styles/GlobalClassNames";
 import AboutMe from "@components/aboutMe/AboutMe";
+import SkillSection from "@components/skillSection/SkillSection";
 
 export default function Page() {
 
@@ -28,44 +22,8 @@ export default function Page() {
 
             <AboutMe />
 
+            <SkillSection />
 
-            {/* skills */}
-            <Grid item xs={12} className={SECTION_TITLE}>
-                <Typography variant="h3" className={style.bold}>
-                    {
-                        STATIC_CONTENT.HOME_PAGE.MY_SKILLSET.TITLE
-                    }
-                </Typography>
-            </Grid>
-            {
-                Object.entries(STATIC_CONTENT.HOME_PAGE.MY_SKILLSET.SKILLSET).map(([skillType, skill], i) => (
-                    <>
-                    <Grid item xs={12}>
-                        <Typography variant="h5" className={style.bold}>
-                            {
-                                titleCase(skillType)
-                            }
-                        </Typography>
-                    </Grid>           
-                    <Grid item key={i} xs={12} className={VERTICALLY_HORIZONTALLY_CENTER}>      
-                        {
-                            skill.map((skillInfo: {name: string, icon:string, url:string}, j: number) => (
-                                <Grid key={`${i}-${j}`} item xs={6} sm={4} md={3} xl={2} className={style.box + " " + style.animatedBox}>
-                                    <div style={{margin:'8px', minHeight:'calc(100% - 16px)', minWidth:'calc(100% - 16px)', backgroundColor: THEME[currentTheme].BACKGROUND}}>
-                                        <div title={skillInfo.name}>
-                                            {
-                                                (skillInfo.icon) ? <Icon icon={skillInfo.icon} style={{fontSize:50}}/> : <Image src={skillInfo.url} alt={skillInfo.name} width={50} height={50} />
-
-                                            }
-                                        </div>
-                                    </div>
-                                </Grid>                                        
-                            ))
-                        }                 
-                    </Grid>                            
-                    </>
-                ))
-            }
         </Grid>
 
     )
---
+++ components/skillSection/SkillSection.tsx
@@ -0,0 +1,64 @@
+
+import { Grid, Typography } from "@mui/material";
+import { titleCase } from "@helper/text";
+import { BOLD, SECTION_TITLE, VERTICALLY_HORIZONTALLY_CENTER } from "@styles/GlobalClassNames";
+import { THEME } from "@theme";
+import { STATIC_CONTENT } from "@constant";
+import style from './SkillSection.module.scss';
+import { ThemeContext, ThemeContextType } from "@context/theme";
+import { getThemeFromContext } from "@helper/theme";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import { useContext } from "react";
+
+function SkillSection() {
+
+
+    const { currentTheme }: ThemeContextType = getThemeFromContext(useContext(ThemeContext));
+
+  return (
+    <>
+          <Grid item xs={12} className={SECTION_TITLE}>
+              <Typography variant="h3" className={BOLD}>
+                  {
+                      STATIC_CONTENT.HOME_PAGE.MY_SKILLSET.TITLE
+                  }
+              </Typography>
+          </Grid>
+          {
+              Object.entries(STATIC_CONTENT.HOME_PAGE.MY_SKILLSET.SKILLSET).map(([skillType, skill], i) => (
+                  <>
+                      <Grid item xs={12}>
+                          <Typography variant="h5" className={BOLD}>
+                              {
+                                  titleCase(skillType)
+                              }
+                          </Typography>
+                      </Grid>
+                      <Grid item key={i} xs={12} className={VERTICALLY_HORIZONTALLY_CENTER}>
+                          {
+                              skill.map((skillInfo: { name: string, icon: string, url: string }, j: number) => (
+                                  <Grid key={`${i}-${j}`} item xs={6} sm={4} md={3} xl={2} className={style.box + " " + style.animatedBox}>
+                                      <div style={{ margin: '8px', minHeight: 'calc(100% - 16px)', minWidth: 'calc(100% - 16px)', backgroundColor: THEME[currentTheme].BACKGROUND }}>
+                                          <div title={skillInfo.name}>
+                                              {
+                                                  (skillInfo.icon) ? 
+                                                    <Icon icon={skillInfo.icon} style={{ fontSize: 50 }} /> 
+                                                  : 
+                                                    <Image src={skillInfo.url} alt={skillInfo.name} width={50} height={50} />
+
+                                              }
+                                          </div>
+                                      </div>
+                                  </Grid>
+                              ))
+                          }
+                      </Grid>
+                  </>
+              ))
+          }    
+    </>
+  )
+}
+
+export default SkillSection
