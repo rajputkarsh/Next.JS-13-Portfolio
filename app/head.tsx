8730705a6dd5e4f5f8b6ece5c1b235bcda8000fe
Utkarsh <r.utkarsh.0010@gmail.com>
2023-07-15 18:04:58 +0530

    update global.scss import

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default async function Head() {
     return (
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href="https://fonts.googleapis.com/css?family=Open Sans:wght@200;300;400;500;600;700;800;900;1000&display=swapp" rel="stylesheet" />
         </>
     )
 }
